4679a4d5356ae8f26b37bb7d827e84eea1a69989
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-11-29 21:03:56 +1100

    fix mesoderm axis code

--- unfold.py
+++ unfold.py
@@ -2,11 +2,21 @@ import numpy as np
 from skimage import morphology
 from skan import csr
 from scipy.sparse import csgraph
+import networkx as nx
+
+
 
 def define_mesoderm_axis(thresholded_image, *, spacing=1):
     skeleton = morphology.skeletonize_3d(thresholded_image)
     graph, idxs, degrees = csr.skeleton_to_csgraph(skeleton, spacing=spacing)
     distances, paths = csgraph.shortest_path(graph, return_predecessors=True)
     maxdist = np.nonzero(distances ==
-                         np.max(distances[np.isfinite(distances)]))[0]
-    return maxdist
+                         np.max(distances[np.isfinite(distances)]))
+    source, target = np.transpose(maxdist)[0]
+    maxpath = nx.shortest_path(nx.from_scipy_sparse_matrix(graph),
+                               source, target, weight='weight')
+    return graph, idxs, maxpath
+
+
+def path_coordinates(idxs, path, sigma=50):
+    idxs_path = idxs[path]
